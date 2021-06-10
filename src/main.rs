c7bfd051e85ee8576ac26bb2d3a1767135cd0863
phillip-kemper <phillip-kemper@gmx.net>
2021-06-11 00:07:18 +0100

    add subscribe endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 //! src/main.rs
-use zero2prod::run;
 use std::net::TcpListener;
+use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    let listener = TcpListener::bind("127.0.0.1:8000")
-        .expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
     run(listener)?.await
 }
