98df62be320f80a43f01008d2f3aa55daff08055
phillip-kemper <phillip-kemper@gmx.net>
2021-06-20 23:08:15 +0200

    add configurations

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 //! src/main.rs
 use std::net::TcpListener;
 use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Failed to bind random port");
     run(listener)?.await
 }
