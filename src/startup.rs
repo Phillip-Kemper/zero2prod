41d1bfa023e44481b3855172ae54f0163804cf8d
phillip-kemper <phillip-kemper@gmx.net>
2021-06-24 23:48:58 +0200

    add data extractor and env file

--- src/startup.rs
+++ src/startup.rs
@@ -3,14 +3,16 @@ use crate::routes::subscriptions::subscribe;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use std::net::TcpListener;
+use sqlx::PgConnection;
 
 // Notice the different signature!
 // We return `Server` on the happy path and we dropped the `async` keyword // We have no .await call, so it is not needed anymore.
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener,connection: PgConnection) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .data(connection)
     })
     .listen(listener)?
     .run();
