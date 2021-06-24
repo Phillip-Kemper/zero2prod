6dcd2a518a27dc238f359c96e9ce2d4684915376
phillip-kemper <phillip-kemper@gmx.net>
2021-06-24 23:49:24 +0200

    format

--- src/startup.rs
+++ src/startup.rs
@@ -2,12 +2,12 @@ use crate::routes::health_check::health_check;
 use crate::routes::subscriptions::subscribe;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
-use std::net::TcpListener;
 use sqlx::PgConnection;
+use std::net::TcpListener;
 
 // Notice the different signature!
 // We return `Server` on the happy path and we dropped the `async` keyword // We have no .await call, so it is not needed anymore.
-pub fn run(listener: TcpListener,connection: PgConnection) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
