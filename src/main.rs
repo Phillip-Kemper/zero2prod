202913b1178f584e2dc3eef28710ab4d4a6492fe
phillip-kemper <phillip-kemper@gmx.net>
2021-06-27 00:56:45 +0200

    add db pool

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 //! src/main.rs
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -8,7 +9,11 @@ async fn main() -> std::io::Result<()> {
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
     let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
